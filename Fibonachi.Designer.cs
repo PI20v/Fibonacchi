1a9448924483f96a6fd4548e0b9550189479b89c
Glek2 <99312596+Glek2@users.noreply.github.com>
2022-06-05 18:16:07 +0300

    Add files via upload

---
+++ Fibonachi.Designer.cs
@@ -0,0 +1,140 @@
+﻿
+namespace Fib
+{
+    partial class Fibonachi
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Fibonachi));
+            this.label5 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.toolStrip1 = new System.Windows.Forms.ToolStrip();
+            this.toolStripButton1 = new System.Windows.Forms.ToolStripButton();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            this.toolStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 13F);
+            this.label5.Location = new System.Drawing.Point(12, 31);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(140, 22);
+            this.label5.TabIndex = 10;
+            this.label5.Text = "Введите число:";
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button1.Location = new System.Drawing.Point(141, 273);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(115, 29);
+            this.button1.TabIndex = 17;
+            this.button1.Text = "Вычислить";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(16, 60);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.ReadOnly = true;
+            this.dataGridView1.Size = new System.Drawing.Size(240, 207);
+            this.dataGridView1.TabIndex = 18;
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F);
+            this.numericUpDown1.Location = new System.Drawing.Point(158, 32);
+            this.numericUpDown1.Maximum = new decimal(new int[] {
+            1410065408,
+            2,
+            0,
+            0});
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(98, 21);
+            this.numericUpDown1.TabIndex = 19;
+            // 
+            // toolStrip1
+            // 
+            this.toolStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripButton1});
+            this.toolStrip1.Location = new System.Drawing.Point(0, 0);
+            this.toolStrip1.Name = "toolStrip1";
+            this.toolStrip1.Size = new System.Drawing.Size(271, 25);
+            this.toolStrip1.TabIndex = 20;
+            this.toolStrip1.Text = "toolStrip1";
+            // 
+            // toolStripButton1
+            // 
+            this.toolStripButton1.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Text;
+            this.toolStripButton1.Image = ((System.Drawing.Image)(resources.GetObject("toolStripButton1.Image")));
+            this.toolStripButton1.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.toolStripButton1.Name = "toolStripButton1";
+            this.toolStripButton1.Size = new System.Drawing.Size(57, 22);
+            this.toolStripButton1.Text = "Справка";
+            this.toolStripButton1.Click += new System.EventHandler(this.toolStripButton1_Click);
+            // 
+            // Fibonachi
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.ClientSize = new System.Drawing.Size(271, 314);
+            this.Controls.Add(this.toolStrip1);
+            this.Controls.Add(this.numericUpDown1);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label5);
+            this.Name = "Fibonachi";
+            this.Text = "Form1";
+            this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.Fibonachi_FormClosed);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            this.toolStrip1.ResumeLayout(false);
+            this.toolStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+        private System.Windows.Forms.ToolStrip toolStrip1;
+        private System.Windows.Forms.ToolStripButton toolStripButton1;
+    }
+}
---
+++ Fibonachi.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Fib
+{
+    public partial class Fibonachi : Form
+    {
+        Autorization autorization;
+        delegate void Del();
+        Del del;
+        List<int> FibCounts = new List<int>();
+        string User;
+        int a;
+
+        public Fibonachi(Autorization autorization, string User)
+        {
+            InitializeComponent();
+            dataGridView1.Columns.Add("Fib", "Ряд Фибоначчи");
+            this.autorization = autorization;
+            this.User = User;
+
+            using (StreamWriter writer = new StreamWriter(User + ".txt", File.Exists(User + ".txt")))
+            {
+                writer.WriteLine("Пользователь авторизировался - " + User + ", время - " + DateTime.Now.ToString().Split(' ')[0]);
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            a = (int)numericUpDown1.Value;
+
+            dataGridView1.Rows.Clear();
+            FibCounts.Clear();
+
+            if (a >= 1) FibCounts.Add(0);
+            if (a >= 2) FibCounts.Add(1);
+            if (a > 3)
+            {
+                del = Fib;
+                BeginInvoke(del);
+            }
+
+            if ((FibCounts.Count == 1 || FibCounts.Count == 2) && (a == 0 || a == 1))
+            {
+                using (StreamWriter writer = new StreamWriter(User + ".txt", File.Exists(User + ".txt")))
+                {
+                    writer.Write("Пользователь ввел число - " + a + " и получил числа Фибоначчи - ");
+                    for (int i = 0; i < FibCounts.Count; i++)
+                    {
+                        dataGridView1.Rows.Add(FibCounts[i].ToString());
+                        writer.Write(FibCounts[i] + "; ");
+                    }
+                    writer.Write('\n');
+                }
+            }
+        }
+
+        private void Fib()
+        {
+            int temp = FibCounts[FibCounts.Count - 1] + FibCounts[FibCounts.Count - 2];
+            if (FibCounts.Count < a)
+            {
+                FibCounts.Add(temp);
+                Fib();
+            }
+            else
+            {
+                using (StreamWriter writer = new StreamWriter(User + ".txt", File.Exists(User + ".txt")))
+                {
+                    writer.Write("Пользователь ввел число - " + a + " и получил числа Фибоначчи - ");
+                    for (int i = 0; i < FibCounts.Count; i++)
+                    {
+                        dataGridView1.Rows.Add(FibCounts[i].ToString());
+                        writer.Write(FibCounts[i] + "; ");
+                    }
+                    writer.Write('\n');
+                }
+            }
+            return;
+        }
+
+        private void Fibonachi_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            autorization.Show();
+        }
+
+        private void toolStripButton1_Click(object sender, EventArgs e)
+        {
+            if (File.Exists("Справка.ref")) Process.Start("Справка.ref");
+            else
+            {
+                MessageBox.Show("Файл со справкой отсутствует", "Ошибка");
+                return;
+            }
+        }
+    }
+}
